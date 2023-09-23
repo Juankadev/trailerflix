a46afc93dd247c1f790c83a9df202b73000f33c9
juancruzrey <juancruzonepiece@gmail.com>
2023-09-23 18:59:34 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,6 +7,12 @@ const { DB_SCHEMA, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 const db = new Sequelize(DB_SCHEMA, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "mysql",
+  pool:{
+    max:5,
+    min:0,
+    acquire:30000,
+    idle:10000,
+  }
 });
 
 // async function authenticate() {
